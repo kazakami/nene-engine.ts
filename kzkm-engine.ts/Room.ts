e0de4fc932dbcd90123a54e77b3f874fd0ee92d7
kazakami <kazakami@kmc.gr.jp>
2018-06-26 19:18:09 +0900

    説明のコメントを追加

--- kzkm-engine.ts/Room.ts
+++ kzkm-engine.ts/Room.ts
@@ -13,6 +13,7 @@ class Room {
     }
 
     Update(): void {
+        //有効でなくなったUnitの削除処理を行ってからUpdate()を実行する
         this.Remove();
         this.units.forEach(u => {
             u.Update();
@@ -32,20 +33,24 @@ class Room {
     }
 
     AddMesh(u: Unit, m: Mesh): void {
+        //Sceneにメッシュを追加し、Unitに紐づける
         this.scene.add(m);
         u.meshes.push(m);
     }
 
     AddUnit(u: Unit): void {
+        //Initを実行してからリストに追加
         u.Init();
         this.units.push(u);
     }
 
     Remove(): void {
+        //有効でなくなったUnitに紐づけられてるメッシュを削除し、Fin()を呼び出す
         this.units.filter(u => !u.isAlive).forEach(u => {
             u.meshes.forEach(m => {this.scene.remove(m)});
             u.Fin();
         });
+        //Unitのリストから有効でなくなったものを取り除く
         this.units = this.units.filter(u => u.isAlive);
     }
 }
