f13e19b4f550a94f9d4d9c87e0b3b9ed5dc4b658
kazakami <kazakami@kmc.gr.jp>
2018-07-10 12:41:33 +0900

    物理演算をクラスに押し込んだ

--- hello.ts
+++ hello.ts
@@ -1,6 +1,7 @@
 import * as Cannon from "cannon";
 import * as THREE from "three";
 import { core, Start } from "./kzkm-engine.ts/Core";
+import { PhysicObject, PhysicPlane, PhysicSphere } from "./kzkm-engine.ts/PhysicObject";
 import { Room } from "./kzkm-engine.ts/Room";
 import { Unit } from "./kzkm-engine.ts/Unit";
 
@@ -23,18 +24,12 @@ class LoadRoom extends Room {
 
 // tslint:disable-next-line:max-classes-per-file
 class GameRoom extends Room {
-    public world: Cannon.World;
-    public groundMat: Cannon.Material;
-    public phyPlane: Cannon.Body;
-    public sphereMat: Cannon.Material;
-    public phySphere: Cannon.Body;
-    public viewSphere: THREE.Mesh;
-    public viewPlane: THREE.Mesh;
     public sprt: THREE.Sprite;
     public Init(): void {
         super.Init();
         this.AddUnit(new Chara());
         this.AddUnit(new ObjTest());
+        this.AddUnit(new Physic());
         this.camera.position.z = 10;
         this.camera.position.y = 10;
         this.camera.lookAt(0, 0, 0);
@@ -46,57 +41,10 @@ class GameRoom extends Room {
         this.sprt.scale.set(100, 100, 1);
         // this.sprt.position.set(500, 500, -1);
         this.scene2d.add(this.sprt);
-        this.world = new Cannon.World();
-        this.world.gravity.set(0, -9.82, 0);
-        this.world.broadphase = new Cannon.NaiveBroadphase();
-        this.world.solver.iterations = 5;
-        this.groundMat = new Cannon.Material("groundMat");
-        this.phyPlane = new Cannon.Body({
-            mass: 0,
-            material: this.groundMat,
-        });
-        this.phyPlane.addShape(new Cannon.Plane());
-        this.phyPlane.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0), -Math.PI / 2);
-        this.world.addBody(this.phyPlane);
-        this.sphereMat = new Cannon.Material("sphereMat");
-        this.phySphere = new Cannon.Body({
-            mass: 1,
-            material: this.sphereMat,
-        });
-        this.phySphere.addShape(new Cannon.Sphere(1));
-        this.phySphere.position.set(0, 10, 0);
-        this.phySphere.angularVelocity.set(0, 0, 0);
-        this.phySphere.angularDamping = 0.1;
-        this.world.addBody(this.phySphere);
-        this.viewSphere = new THREE.Mesh(
-            new THREE.SphereGeometry(1, 50, 50),
-            new THREE.MeshLambertMaterial(
-                {
-                    color: 0xffffff,
-                }));
-        this.scene.add(this.viewSphere);
-        this.viewPlane = new THREE.Mesh(
-            new THREE.PlaneGeometry(300, 300),
-            new THREE.MeshPhongMaterial(
-                {
-                    color: 0x333333,
-                }));
-        this.viewPlane.rotation.x = -Math.PI / 2;
-        this.viewPlane.rotation.y = 0;
-        this.scene.add(this.viewPlane);
     }
     public Update(): void {
         super.Update();
         this.sprt.position.set(core.mouseX, core.mouseY, -1);
-        this.world.step(1 / 60);
-        this.viewSphere.position.x = this.phySphere.position.x;
-        this.viewSphere.position.y = this.phySphere.position.y;
-        this.viewSphere.position.z = this.phySphere.position.z;
-
-        this.viewSphere.quaternion.w = this.phySphere.quaternion.w;
-        this.viewSphere.quaternion.x = this.phySphere.quaternion.x;
-        this.viewSphere.quaternion.y = this.phySphere.quaternion.y;
-        this.viewSphere.quaternion.z = this.phySphere.quaternion.z;
         // if (this.frame % 100 == 0) console.log(this.core);
         // console.log(this.core.mouseX + ", " + this.core.mouseY);
     }
@@ -159,6 +107,23 @@ class Chara extends Unit {
     public Fin(): void {
         this.geometry.dispose();
         this.material.dispose();
+    }
+}
+
+class Physic extends Unit {
+    public Draw(): void {
+        return;
+    }
+    public Init(): void {
+        const plane = new PhysicPlane(0);
+        const sphere = new PhysicSphere(1, 1);
+        plane.PhyBody.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0), -Math.PI / 2);
+        sphere.PhyBody.position.set(0, 10, 0);
+        this.AddPhysicObject(plane);
+        this.AddPhysicObject(sphere);
+    }
+    public Fin(): void {
+        return;
     }
 }
 
