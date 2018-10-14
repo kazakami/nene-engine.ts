f22aba5143631141d5bc35cb20f4cfcda9ec4ed1
kazakami <kazakami@kmc.gr.jp>
2018-10-15 06:47:25 +0900

    物理演算の確認用に追加していたコードを削除

--- hello.ts
+++ hello.ts
@@ -1,9 +1,7 @@
 import * as Cannon from "cannon";
 import * as THREE from "three";
 import { core, Start } from "./kzkm-engine.ts/src/Core";
-import { PhysicBox, PhysicObject, PhysicPlane, PhysicSphere } from "./kzkm-engine.ts/src/PhysicObject";
 import { Room } from "./kzkm-engine.ts/src/Room";
-import { Unit } from "./kzkm-engine.ts/src/Unit";
 
 class LoadRoom extends Room {
     public Init(): void {
@@ -26,8 +24,7 @@ class GameRoom extends Room {
     public sprt: THREE.Sprite;
     public Init(): void {
         super.Init();
-        this.LoadFromFile("resources/OnePhysicObject.json");
-        // this.AddUnit(new Physic());
+        this.LoadFromFile("resources/PhysicObjects.json");
         this.camera.position.z = 10;
         this.camera.position.y = 10;
         this.camera.lookAt(0, 0, 0);
@@ -42,41 +39,6 @@ class GameRoom extends Room {
     public Update(): void {
         super.Update();
         this.sprt.position.set(core.mouseX, core.mouseY, -1);
-    }
-}
-
-class Physic extends Unit {
-    public plane: PhysicPlane;
-    public Draw(): void {
-        return;
-    }
-    public Init(): void {
-        this.plane = new PhysicPlane(0);
-        const sphere1 = new PhysicSphere(1, 1);
-        const sphere2 = new PhysicSphere(1, 1);
-        const sphere3 = new PhysicSphere(1, 1);
-        const box1 = new PhysicBox(1, 1, 1, 1);
-        const box2 = new PhysicBox(1, 1, 1, 1);
-        this.plane.PhyBody.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0), -Math.PI / 2);
-        sphere1.PhyBody.position.set(0, 10, 0);
-        sphere2.PhyBody.position.set(1, 8, 0);
-        sphere3.PhyBody.position.set(0, 6, 1);
-        box1.PhyBody.position.set(1, 4, 1);
-        box2.PhyBody.position.set(1, 2, 1);
-        this.AddPhysicObject(this.plane);
-        this.AddPhysicObject(sphere1);
-        this.AddPhysicObject(sphere2);
-        this.AddPhysicObject(sphere3);
-        this.AddPhysicObject(box1);
-        this.AddPhysicObject(box2);
-    }
-    public Update(): void {
-        super.Update();
-        this.plane.PhyBody.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0),
-          -Math.PI / 2 - 0.1 + 0 * Math.sin(this.frame / 100.0) / 5.0);
-    }
-    public Fin(): void {
-        return;
     }
 }
 
