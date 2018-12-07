569b352e8c85e66c82c657b931d213abc3e5714c
kazakami <kazakami@kmc.gr.jp>
2018-12-07 16:54:07 +0900

    地形の座標を指定して高度変更できるようにした

--- examples/terrain.ts
+++ examples/terrain.ts
@@ -26,8 +26,14 @@ class GameScene extends Scene {
         this.backgroundColor = new THREE.Color(0.6, 0.8, 0.9);
         this.scene.fog = new THREE.Fog(new THREE.Color(0.6, 0.8, 0.9).getHex(), 1, 3000);
         this.t = new Terrain();
-        this.t.MakeGeometry(100, 100, 100, 100);
+        this.t.MakeGeometry(50, 50, 100, 100);
         this.scene.add(this.t.GetObject());
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                this.t.SetHeight(i, j, Math.random() * 10, false);
+            }
+        }
+        this.t.ComputeNorm();
         const light = new THREE.DirectionalLight("white", 1);
         light.position.set(50, 100, 50);
         this.scene.add(light);
--- src/Terrain.ts
+++ src/Terrain.ts
@@ -4,18 +4,29 @@ export class Terrain {
     private geo: THREE.PlaneBufferGeometry;
     private scene: THREE.Scene = new THREE.Scene();
     private heights: number[][];
+    private segX: number;
+    private segY: number;
     public MakeGeometry(x: number, y: number, segX: number, segY: number): void {
-        this.geo = new THREE.PlaneBufferGeometry(x, y, segX, segY);
+        this.segX = segX;
+        this.segY = segY;
+        this.geo = new THREE.PlaneBufferGeometry(x, y, segX - 1, segY - 1);
         this.geo.rotateX(- Math.PI / 2);
-        const mat = new THREE.MeshPhongMaterial({color: 0x000000});
+        const mat = new THREE.MeshPhongMaterial({color: 0x888888});
         const terrain = new THREE.Mesh(this.geo, mat);
         this.scene.add(terrain);
     }
     public GetObject(): THREE.Object3D {
         return this.scene;
     }
-    public SetHeight(x: number, y: number, height: number): void {
+    public SetHeight(x: number, y: number, height: number, computeNorm: boolean = true): void {
+        this.geo.attributes.position.setY(y * this.segX + x, height);
+        if (computeNorm) {
+            this.geo.computeVertexNormals();
+        }
         return;
+    }
+    public ComputeNorm(): void {
+        this.geo.computeVertexNormals();
     }
     public GetHeight(x: number, y: number): number {
         return 0;
