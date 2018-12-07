ec92f31de624eeabfe73f8ad93bcb91bacd5b202
kazakami <kazakami@kmc.gr.jp>
2018-12-07 16:16:16 +0900

    examplesにterrainを追加

---
+++ examples/terrain.ts
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+import { Scene, Start, Terrain } from "../src/nene-engine";
+
+class LoadScene extends Scene {
+    public Init() {
+        this.core.LoadTexture("resources/images/grass.png", "grass");
+    }
+    public Update(): void {
+        if (this.core.IsAllResourcesAvailable()) {
+            // オブジェクトenteが読み込まれればシーン遷移
+            this.core.AddAndChangeScene("game", new GameScene());
+        }
+    }
+    public DrawText(): void {
+        const [a, b] = this.core.GetAllResourcesLoadingProgress();
+        this.core.DrawText("Now Loading " + a + "/" + b, 0, 0);
+    }
+}
+
+class GameScene extends Scene {
+    private t: Terrain;
+    public Init() {
+        this.onWindowResize = () => {
+            this.core.ChangeCanvasSize(window.innerWidth, window.innerHeight);
+        };
+        this.backgroundColor = new THREE.Color(0.6, 0.8, 0.9);
+        this.scene.fog = new THREE.Fog(new THREE.Color(0.6, 0.8, 0.9).getHex(), 1, 3000);
+        this.t = new Terrain();
+        this.t.MakeGeometry(100, 100, 100, 100);
+        this.scene.add(this.t.GetObject());
+        const light = new THREE.DirectionalLight("white", 1);
+        light.position.set(50, 100, 50);
+        this.scene.add(light);
+        this.camera.position.set(50, 50, 50);
+        this.camera.up.set(0, 1, 0);
+        this.camera.lookAt(0, 0, 0);
+    }
+}
+
+const c = Start("initScene", new LoadScene());
