c51e6e17424362228b1526c63764161b441a02dd
kazakami <kazakami@kmc.gr.jp>
2019-02-22 17:28:55 +0900

    パーティクルのラッパどうしよ

--- src/Particles.ts
+++ src/Particles.ts
@@ -1,17 +1,28 @@
 import * as THREE from "three";
 
-class Particles {
+export class Particles {
     private geo: THREE.Geometry;
     private particlesNum: number;
     private mat: THREE.PointsMaterial;
     private points: THREE.Points;
-    constructor(num: number) {
+    private updateFunc: (x: number, y: number, z: number) => [number, number, number];
+    constructor(pos: THREE.Vector3, num: number) {
         this.geo = new THREE.Geometry();
         this.particlesNum = num;
         for (let i = 0; i < this.particlesNum; i++) {
-            this.geo.vertices.push(new THREE.Vector3());
+            this.geo.vertices.push(pos.clone());
         }
         this.mat = new THREE.PointsMaterial({});
         this.points = new THREE.Points(this.geo, this.mat);
+    }
+
+    public get particle(): THREE.Points {
+        return this.points;
+    }
+    public Update(): void {
+        for (const vec of this.geo.vertices) {
+            const [x, y, z] = this.updateFunc(vec.x, vec.y, vec.z);
+            vec.set(x, y, z);
+        }
     }
 }
