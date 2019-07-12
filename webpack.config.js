321efe6d480bd7aebed14be8d593518561db6559
kazakami <kazakami@kmc.gr.jp>
2019-07-12 13:36:04 +0900

    エディタのCLIプログラムを書き始め、ビルド対象に追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 module.exports = {
-    mode: 'development',
+  mode: 'development',
 
-    entry: {
-      'balls': './examples/balls.ts',
-      '2D': './examples/2D.ts',
-      'flight': './examples/flight.ts',
-      'flightWorker': './examples/flightWorker.ts',
-      'terrain': './examples/terrain.ts',
-      'scenes': './examples/scenes.ts',
-    },
+  entry: {
+    'lib/dist/cli': './src/Neneditor.ts',
+    'examples/dist/balls': './examples/balls.ts',
+    'examples/dist/2D': './examples/2D.ts',
+    'examples/dist/flight': './examples/flight.ts',
+    'examples/dist/flightWorker': './examples/flightWorker.ts',
+    'examples/dist/terrain': './examples/terrain.ts',
+    'examples/dist/scenes': './examples/scenes.ts',
+  },
 
-    output: {
-      path: `${__dirname}/examples/dist`,
-      filename: '[name].js'
-    },
-    module: {
-      rules: [
-        {test: /\.ts$/,use: 'ts-loader'}
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    node: {
-      fs: 'empty'
-    }
-  };
+  output: {
+    path: `${__dirname}`,
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  node: {
+    fs: 'empty'
+  }
+};
