c1549379bd16ad0f53b1513c7ec158c7e9d8df01
kazakami <kazakami@kmc.gr.jp>
2019-07-12 15:06:22 +0900

    簡単なサーバ

--- src/Neneditor.ts
+++ src/Neneditor.ts
@@ -1,1 +1,10 @@
-console.log("hoge");
+import http = require("http");
+
+const s = http.createServer((req, res) => {
+    console.log(req.url);
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+    });
+    res.end("hoge\n");
+});
+s.listen(8080);
