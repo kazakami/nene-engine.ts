82bcad4ab9a86b0df1546b93b48efb7b4d6aa0b3
kazakami <kazakami@kmc.gr.jp>
2019-07-16 18:23:42 +0900

    express.js ejsを試してみる

--- src/Neneditor.ts
+++ src/Neneditor.ts
@@ -1,10 +1,17 @@
-import http = require("http");
 
-const s = http.createServer((req, res) => {
-    console.log(req.url);
-    res.writeHead(200, {
-        "Content-Type": "text/html",
+import * as Express from "express";
+const app = Express();
+// tslint:disable-next-line: no-var-requires
+app.engine("ejs", require("ejs").__express);
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+    res.render(__dirname + "/../../lib/hoge.ejs", {
+        content: "ほげ<br/>ふが",
+        title: "タイトル",
     });
-    res.end("hoge\n");
 });
-s.listen(8080);
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+    console.log(__dirname);
+});
