9e9b77cc3ea11db146ee130f5721df85934fd220
kazakami <kazakami@kmc.gr.jp>
2019-08-18 20:58:45 +0900

    @types/webpackを導入した サーバとサンプルでwebpackは別の設定ファイルを参照するようにした

---
+++ server.webpack.config.js
@@ -0,0 +1,25 @@
+// editorのサーバ側のビルド
+/** @type import('webpack').Configuration */
+module.exports = {
+  mode: 'development',
+  entry: {
+    'cli': './src/editor/server/Neneditor.ts',
+  },
+  target: 'node',
+  output: {
+    path: `${__dirname}/lib/dist/server`,
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  }
+};
